830c06e2fe6faae7472c25e5d701d66022087a53
LaggerIsME <abajsadykov2003@gmail.com>
2022-11-08 03:17:21 +0600

    logout() and profile()

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from models import User, db
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:12345678@localhost:5432/history"
+app.config['SECRET_KEY'] = 'superpupersecretkey'
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+
 @app.route('/')
 def index():  # put application's code here
+    if 'email' in session and 'password' in session:
+        logout()
     return render_template('index.html')
 
 
@@ -25,12 +31,18 @@ def price():
 def sign():
     if request.method == "POST":
         email = request.form['email']
+        first_name = request.form['first_name']
+        second_name = request.form['second_name']
+        country = request.form['country']
+        date_of_birth = request.form['date_of_birth']
         password1 = request.form['password']
         password2 = request.form['repeat_password']
         if str(password2) != str(password1):
             return redirect(url_for('sign'))
         else:
-            response = User(email = email, password = password1)
+            response = User(email=email, password=password1,
+                            first_name=first_name, second_name=second_name,
+                            country=country, date_of_birth=date_of_birth)
             db.session.add(response)
             db.session.commit()
             return render_template('login.html')
@@ -41,8 +53,33 @@ def sign():
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        return redirect('index.html')
-    return render_template('login.html')
+        email = request.form['email']
+        password = request.form['password']
+        check = User.query.filter_by(email=email, password=password).all()
+        # Если такого пользователя нет
+        if check is None:
+            return redirect(url_for('login'))
+        else:
+            # хранить пользователя
+            session['email'] = email
+            session['password'] = password
+            return redirect(url_for('profile'))
+    else:
+        return render_template('login.html')
+
+
+@app.route('/profile', methods=["GET", "POST"])
+def profile():
+    infos = User.query.filter_by(email=session['email'], password=session['password']).all()
+    return render_template('profile.html', infos=infos)
+
+
+@app.route('/logout')
+def logout():
+    #Очистить токены
+    session.pop('email', None)
+    session.pop('password', None)
+    return render_template('index.html') 
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date
 
 db = SQLAlchemy()
 
@@ -7,5 +7,12 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(Integer, primary_key=True)
-    email = db.Column(String(200), nullable=False)
+    email = db.Column(String(200), nullable=False, unique = True)
     password = db.Column(String(200), nullable=False)
+    first_name = db.Column(String(200), nullable=False)
+    second_name = db.Column(String(200), nullable=False)
+    country = db.Column(String(200), nullable=False)
+    date_of_birth = db.Column(Date, nullable=False)
+
+
+
